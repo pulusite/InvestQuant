cc90ee9ee8f60fcae44c0be6f9faed773dca5e62
pulusite <pulusite@126.com>
2017-11-05 15:00:41 +0800

    update

--- leetcode/leetTest.py
+++ leetcode/leetTest.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
-print "Hello World!";
-
-#!/usr/bin/python
 import GG066
-print "Hello World!";
+print "Hello World!"
 
-# Solution s1,s2
+myString ='hi,python'
+print myString;
 
+import sys
+print sys.platform
+
+# user = raw_input('Enter login name:')
+# print 'Your login is:',user;
+
+print range(3)
+
+
+# filename=raw_input('Enter file name:')
+# fobj=open(filename,'r')
+# for eachLine in fobj:
+#     print eachLine;
+# fobj.close()
+
+def addMe2Me(x):
+    return (x+x)
+print addMe2Me('python')
 

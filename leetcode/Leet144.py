b9981a6430e1aeb0e3f5502e1b7fd2ec6c7d78e4
pulusite <pulusite@126.com>
2017-11-15 20:59:59 +0800

    update

---
+++ leetcode/Leet144.py
@@ -0,0 +1,20 @@
+import TreeNode
+class Leet144:
+    def preorderTraversal(self, root):
+        ret = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
+
+leet = Leet144
+root = TreeNode(1)
+treeNode2=TreeNode(2)
+treeNode3=TreeNode(3)
+
+print leet.preorderTraversal(leet,root)
+
